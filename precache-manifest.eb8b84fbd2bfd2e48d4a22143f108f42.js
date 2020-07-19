7cbccc73cfd909199ef097957959c95acc1b5fc7
kkatsi <kotsoulis@gmail.com>
2020-07-19 21:34:19 +0300

    Updates

---
+++ precache-manifest.eb8b84fbd2bfd2e48d4a22143f108f42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd22ed943c516ddb799dd63b130e3c89",
+    "url": "/reactcalculator/index.html"
+  },
+  {
+    "revision": "7235c94ccf798e471a4b",
+    "url": "/reactcalculator/static/css/main.59d38227.chunk.css"
+  },
+  {
+    "revision": "d92ccb0cb82ce861982c",
+    "url": "/reactcalculator/static/js/2.c521866d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactcalculator/static/js/2.c521866d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7235c94ccf798e471a4b",
+    "url": "/reactcalculator/static/js/main.5c541e5b.chunk.js"
+  },
+  {
+    "revision": "b166217649897b9f4540",
+    "url": "/reactcalculator/static/js/runtime-main.1cda0b84.js"
+  }
+]);
